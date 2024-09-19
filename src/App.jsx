ebbce47bb453466a0635841e522e79b3624dd6f4
charissaida <charis.saida@gmail.com>
2024-09-19 13:28:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portofolio";
+import Skills from "./components/Skills";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Portfolio />
+      <Experience />
+      <Contact />
+    </>
+  );
+}
